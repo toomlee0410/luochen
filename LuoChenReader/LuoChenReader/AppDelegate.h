23b909c9390359b46314766026952fc56e7af49c
toomlee <toomlee0410@sina.com>
2015-12-29 16:52:58 +0800

    create project

---
+++ LuoChenReader/LuoChenReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LuoChenReader
+//
+//  Created by toomlee on 15/12/29.
+//  Copyright © 2015年 luochen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LuoChenReader/LuoChenReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LuoChenReader
+//
+//  Created by toomlee on 15/12/29.
+//  Copyright © 2015年 luochen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
